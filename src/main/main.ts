b320bdfcce5cf772ddffbe94792b8d77f4e061cf
Halfpint <alexander.sims92@gmail.com>
2023-05-29 22:00:53 +0100

    Remove auto updater

--- src/main/main.ts
+++ src/main/main.ts
@@ -109,7 +109,7 @@ const createWindow = async () => {
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  new AppUpdater();
+  // new AppUpdater();
 };
 
 /**
